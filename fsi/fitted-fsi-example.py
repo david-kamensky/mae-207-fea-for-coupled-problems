5dc0cb68e5a3c300bd4e9b7b1c66b5715e5d38a6
David Kamensky <david.kamensky@gmail.com>
2020-10-02 11:04:14 -0700

    Fixed typo in comments.

--- fsi/fitted-fsi-example.py
+++ fsi/fitted-fsi-example.py
@@ -1,16 +1,12 @@
 """
 Simple example of body-fitted FSI.  This code largely follows the notational
-conventions of Bazilevs et al.,
-
-  https://www.oden.utexas.edu/media/reports/2008/0816.pdf ,
-
-as covered in lecture.  The basic problem setup is a cantilever beam
-in a channel with flow around it.  The flow has no-slip boundary conditions
-on the top and bottom, and is driven by a periodic-in-time Dirichlet 
-boundary condition on the left end.  The right end is a stable Neumann
-boundary condition, using the Navier--Stokes extension of the stable
-Neumann boundary condition for advection--diffusion discussed in lecture.
-(Cf. the technical report by Hughes and Wells linked to by the course site.)  
+conventions from Chapter 4 of the course notes.  The basic problem setup 
+is a cantilever beam in a channel with flow around it.  The flow has 
+no-slip boundary conditions on the top and bottom, and is driven by a 
+periodic-in-time Dirichlet boundary condition on the left end.  
+The right end is a stable Neumann boundary condition, using the 
+Navier--Stokes extension of the stable Neumann boundary condition for 
+advection--diffusion discussed in lecture. 
 
 Some notes:
 
@@ -35,7 +31,6 @@ Some notes:
 from dolfin import *
 from ufl import indices, Jacobian, Min
 from mshr import *
-
 
 ####### Domain and mesh setup #######
 
@@ -75,8 +70,9 @@ h = CellDiameter(mesh)
 # Define subdomains for use in boundary condition definitions:
 class Walls(SubDomain):
     def inside(self, x, on_boundary):
-        return (on_boundary and ((x[1] < DOLFIN_EPS) 
-                                 or (x[1] > (OMEGA_H - DOLFIN_EPS))))
+        return (on_boundary 
+                and ((x[1] < DOLFIN_EPS) 
+                     or (x[1] > (OMEGA_H - DOLFIN_EPS))))
 class Inflow(SubDomain):
     def inside(self, x, on_boundary):
         return (on_boundary and (x[0] < DOLFIN_EPS))
@@ -108,9 +104,10 @@ markers = MeshFunction('size_t', mesh, d, mesh.domains())
 bdry_markers = MeshFunction('size_t', mesh, d-1, 0)
 OUTFLOW_FLAG = 2
 Outflow().mark(bdry_markers,OUTFLOW_FLAG)
-dx = dx(metadata={'quadrature_degree': 2},subdomain_data=markers)
-ds = ds(metadata={'quadrature_degree': 2},subdomain_data=bdry_markers)
-
+dx = dx(metadata={'quadrature_degree': 2},
+        subdomain_data=markers)
+ds = ds(metadata={'quadrature_degree': 2},
+        subdomain_data=bdry_markers)
 
 ####### Elements and function spaces #######
 
@@ -121,10 +118,10 @@ Qe = FiniteElement("CG", cell, 1)
 VQe = MixedElement((Ve,Qe))
 # Mixed function space for velocity and pressure:
 W = FunctionSpace(mesh,VQe)
-# Function space for mesh displacement field, which will be solved
-# for separately in a quasi-direct scheme:
+# Function space for mesh displacement field, 
+# which will be solved for separately in a 
+# quasi-direct scheme:
 V = FunctionSpace(mesh,Ve)
-
 
 ####### Set up time integration variables #######
 
@@ -147,24 +144,28 @@ v_old, p_old = split(w_old)
 dv_dr = (v - v_old)/Dt
 dv_ds = dv_dr # (Only valid in solid)
 
-# This is the displacement field used in the solid formulation; notice that it
-# is NOT in the space V; it is an algebraic object involving the unknown
-# fluid--structure velocity field v.
+# This is the displacement field used in the 
+# solid formulation; notice that it is NOT 
+# in the space V; it is an algebraic object 
+# involving the unknown fluid--structure velocity 
+# field v.
 u = uhat_old + Dt*v
 
-# This will need to be updated to match u, for purposes of setting the
-# boundary condition on the mesh motion subproblem.
+# This will need to be updated to match u, for 
+# purposes of setting the boundary condition 
+# on the mesh motion subproblem.
 u_func = Function(V)
-
 
 ####### Changes of variable #######
 
-# Follow notation from Bazilevs et al., where y is the coordinate in the
-# reference domain, x is the coordinate in the spatial domain, and X is
-# the coordinate in the material domain.  Note that the built-in differential
-# operators (e.g., grad, div, etc.) and integration measures
-# (e.g., dx, ds, etc.) are w.r.t. the reference configuration, y, which
-# is the mesh that FEniCS sees.  
+# Follow notation from Bazilevs et al., where y is 
+# the coordinate in the reference domain, x is the 
+# coordinate in the spatial domain, and X is the 
+# coordinate in the material domain.  Note that 
+# the built-in differential operators (e.g., grad, 
+# div, etc.) and integration measures (e.g., dx, ds, 
+# etc.) are w.r.t. the reference configuration, y, 
+# which is the mesh that FEniCS sees.  
 dX = dx(SOLID_FLAG)
 dy = dx
 grad_y = grad
@@ -180,23 +181,26 @@ def div_x_tens(f): # (For (rank-2) tensor-valued f)
     i,j = indices(2)
     return as_tensor(grad_x(f)[i,j,j],(i,))
 
-# Note:  Trying to define dx = det_dxdy*dy would result in an object
-# of type Form, which could no longer be used as an integration measure.
-# Thus, integration over the spatial configuration is done with
-# det_dxdy*dy directly in the fluid formulation.  
-
+# Note:  Trying to define dx = det_dxdy*dy would 
+# result in an object of type Form, which could no 
+# longer be used as an integration measure.
+# Thus, integration over the spatial configuration 
+# is done with det_dxdy*dy directly in the fluid 
+# formulation.  
 
 ####### Boundary conditions #######
 
 # BCs for the fluid--structure subproblem:
 bc0_fs = DirichletBC(W.sub(0), Constant((0.0,0.0)), Walls())
-# Note that "x" in this Expression is really y in the kinematics
-# described above, but, because the mesh motion problem has a zero
-# Dirichlet BC on the inflow boundary, there happens to be no difference.
+# Note that "x" in this Expression is really y 
+# in the kinematics described above, but, because the 
+# mesh motion problem has a zero Dirichlet BC on the inflow
+# boundary, there happens to be no difference.
 v_in = Expression(("2.0*sin(pi*t)*x[1]*(H - x[1])/(H*H)",
                    "0.0"),t=0.0,H=OMEGA_H,degree=2)
 bc1_fs = DirichletBC(W.sub(0), v_in, Inflow())
-bc2_fs = DirichletBC(W.sub(1), Constant(0), SolidDomainInterior())
+bc2_fs = DirichletBC(W.sub(1), Constant(0), 
+                     SolidDomainInterior())
 bcs_fs = [bc0_fs, bc1_fs, bc2_fs]
 
 # BCs for the mesh motion subproblem:
@@ -209,7 +213,7 @@ bcs_m = [bc_m_struct,bc_m_walls,bc_m_inflow,bc_m_outflow]
 
 ####### Formulation of mesh motion subproblem #######
 
-# Residual for mesh, which satisfies a fictious elastic problem:
+# Residual for mesh, which satisfies a fictitious elastic problem:
 F_m = grad_y(uhat) + I
 E_m = 0.5*(F_m.T*F_m - I)
 m_jac_stiff_pow = Constant(3)
@@ -233,8 +237,8 @@ F = grad_X(u) + I
 E = 0.5*(F.T*F - I)
 S = K*tr(E)*I + 2.0*mu_s*(E - tr(E)*I/3.0)
 f_s = Constant(d*(0,))
-res_s = rho_s0*inner(dv_ds - f_s,dv)*dX + inner(F*S,grad_X(dv))*dX
-
+res_s = rho_s0*inner(dv_ds - f_s,dv)*dX \
+        + inner(F*S,grad_X(dv))*dX
 
 ####### Formulation of the fluid subproblem #######
 
@@ -257,7 +261,8 @@ G = (dxi_dx.T)*dxi_dx
 # SUPG/PSPG:
 resStrong_f = rho_f*DvDt - div_x_tens(sigma_f)
 Cinv = Constant(1.0)
-tau_M = 1.0/sqrt(((2*rho_f/Dt)**2) + inner(rho_f*v_adv,G*(rho_f*v_adv))
+tau_M = 1.0/sqrt(((2*rho_f/Dt)**2) 
+                 + inner(rho_f*v_adv,G*(rho_f*v_adv))
                  + Cinv*(mu_f**2)*inner(G,G))
 resSUPG_f = inner(tau_M*resStrong_f,
                   rho_f*dot(grad_x(dv),v_adv)
@@ -266,15 +271,16 @@ resSUPG_f = inner(tau_M*resStrong_f,
 tau_C = 1.0/(tr(G)*tau_M)
 resLSIC_f = tau_C*div_x(v)*div_x(dv)*det_dxdy*dy(FLUID_FLAG)
 
-# Stable Neumann BC term, assuming advective form of material time
-# derivative term:
+# Stable Neumann BC term, assuming advective 
+# form of material time derivative term:
 v_adv_minus = Min(dot(v_adv,n),Constant(0))
 resOutflow_f = -dot(rho_f*v_adv_minus*dv,v)*ds(OUTFLOW_FLAG)
 
-# Note: On a general deforming mesh, the boundary integration measure
-# would need to be scaled using Nanson's formula, but, here, the outflow
-# boundary is fixed so we can directly use the ds measure corresponding
-# to the reference domain.
+# Note: On a general deforming mesh, the boundary 
+# integration measure ds would need to be scaled using Nanson's 
+# formula, but, here, the outflow boundary is fixed so we 
+# can directly use the ds measure corresponding to the 
+# reference domain.
 
 # Full fluid residual
 res_f = resGal_f + resSUPG_f + resLSIC_f + resOutflow_f
@@ -286,8 +292,9 @@ Dres_fs = derivative(res_fs, w)
 
 ####### Nonlinear solver setup #######
 
-# Nonlinear solver parameters; set relative tolerances for subproblems
-# tighter than tolerance for coupled problem, to prevent stagnation.
+# Nonlinear solver parameters; set relative tolerances 
+# for subproblems tighter than tolerance for coupled problem, 
+# to prevent stagnation.
 REL_TOL_FSM = 1e-3
 REL_TOL_FS = REL_TOL_M = REL_TOL_FSM*1e-1
 MAX_ITERS_FSM = 100
@@ -295,17 +302,23 @@ MAX_ITERS_M = 100
 MAX_ITERS_FS = 100
 
 # Set up nonlinear problem for mesh motion:
-problem_m = NonlinearVariationalProblem(res_m, uhat, bcs_m, Dres_m)
+problem_m = NonlinearVariationalProblem(res_m, uhat, 
+                                        bcs_m, Dres_m)
 solver_m = NonlinearVariationalSolver(problem_m)
-solver_m.parameters['newton_solver']['maximum_iterations'] = MAX_ITERS_M
-solver_m.parameters['newton_solver']['relative_tolerance'] = REL_TOL_M
+solver_m.parameters['newton_solver']\
+                   ['maximum_iterations'] = MAX_ITERS_M
+solver_m.parameters['newton_solver']\
+                   ['relative_tolerance'] = REL_TOL_M
 
-# Create variational problem and solver for the fluid--structure problem:
-problem_fs = NonlinearVariationalProblem(res_fs, w, bcs_fs, Dres_fs)
+# Create variational problem and solver for 
+# the fluid--structure problem:
+problem_fs = NonlinearVariationalProblem(res_fs, w, 
+                                         bcs_fs, Dres_fs)
 solver_fs = NonlinearVariationalSolver(problem_fs)
-solver_fs.parameters['newton_solver']['maximum_iterations'] = MAX_ITERS_FS
-solver_fs.parameters['newton_solver']['relative_tolerance'] = REL_TOL_FS
-
+solver_fs.parameters['newton_solver']\
+                    ['maximum_iterations'] = MAX_ITERS_FS
+solver_fs.parameters['newton_solver']\
+                    ['relative_tolerance'] = REL_TOL_FS
 
 ####### Time stepping loop #######
 
@@ -330,12 +343,14 @@ while t < TIME_INTERVAL:
     # Use the current time in the inflow BC definition.
     v_in.t = t
 
-    # "Quasi-direct" coupling: the fluid and structure are solved in one
-    # system, but the mesh is solved in a separate block.
+    # "Quasi-direct" coupling: the fluid and structure 
+    # are solved in one system, but the mesh is solved 
+    # in a separate block.
     for i in range(0,MAX_ITERS_FSM):
 
-        # Check fluid--structure residual on the moved mesh, and
-        # terminate iteration if this residual is small:
+        # Check fluid--structure residual on the moved
+        # mesh, and terminate iteration if this residual 
+        # is small:
         res_fs_vec = assemble(res_fs)
         for bc in bcs_fs:
             bc.apply(res_fs_vec,w.vector())
@@ -350,15 +365,19 @@ while t < TIME_INTERVAL:
         if(res_rel < REL_TOL_FSM):
             break
 
-        # Solve for fluid/structure velocity and pressure at current time:
+        # Solve for fluid/structure velocity and 
+        # pressure at current time:
         solver_fs.solve()
         
-        # Update Function in V to be used in mesh motion BC.  (There are
-        # workarounds to avoid this projection (which requires a linear
-        # solve), but projection is most concise for illustration.)
+        # Update Function in V to be used in mesh 
+        # motion BC.  (There are workarounds to avoid 
+        # this projection (which requires a linear
+        # solve), but projection is most concise for 
+        # illustration.)
         u_func.assign(project(u,V))
 
-        # Mesh motion problem; updates uhat at current time level:
+        # Mesh motion problem; updates uhat at current 
+        # time level:
         solver_m.solve()
     
     # Extract solutions:
